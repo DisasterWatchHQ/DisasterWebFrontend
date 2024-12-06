add3fa854cde6f9d79494b97c7e03f016e77b860
Dinuki85 <prarthanadinuki2@gmail.com>
2024-12-06 09:49:36 +0530

    refactor: update Login Page with improved structure and functionality

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,7 @@
-import Footer from "../components/Footer";
-import LoginForm from "../components/LoginForm";
-
+"use client";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import LoginForm from "@/components/LoginForm";
 export default function Login(){
     return(
         <div className="flex flex-col min-h-screen">
