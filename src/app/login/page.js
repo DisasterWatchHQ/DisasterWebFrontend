63a8caa82498c7b13e218ce89b850afe0f9ebf79
5yndr0m <sdilanjana21@gmail.com>
2024-12-07 21:53:58 +0530

    fix: removed extra use of headers and footers

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -5,14 +5,12 @@ import LoginForm from "@/components/LoginForm";
 export default function Login(){
     return(
         <div className="flex flex-col min-h-screen">
-            <Header/>
             <main className="flex-1 flex items-center justify-center bg-gray-100">
                 <div className="w-full max-w-md bg-white p-6 rounded shadow">
                     <h1 className="text-2xl font-bold mb-4 text-center">Sign In</h1>
                     <LoginForm />
                 </div>
             </main>
-            <Footer/>
         </div>
     );
 }
