f16e0e0caa439c67a804fdc478c221c7cbd29aad
5yndr0m <sdilanjana21@gmail.com>
2024-12-25 13:54:45 +0530

    fix: trying to fix hydration error

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,7 @@ export default function RootLayout({ children }) {
   const footer = <Footer />;
 
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col min-h-screen`}
       >
