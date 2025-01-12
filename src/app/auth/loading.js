ba15270288167fb7c32fdb43f9b03b718a69525a
Dinuki85 <prarthanadinuki2@gmail.com>
2025-01-12 19:46:17 +0530

    chore: format code in loading.js for improved readability

--- src/app/auth/loading.js
+++ src/app/auth/loading.js
@@ -1,8 +1,8 @@
-export default function LoginLoading(){
+export default function LoginLoading() {
     return (
         <div className="flex flex items-center justify-center min-h-[400px]">
             <div className="w-16 h-16 animate-spin rounded-full border-b-2 border-gray-900">
-                
+
             </div>
         </div>
     );
