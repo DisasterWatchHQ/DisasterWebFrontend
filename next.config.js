e36e6b974d587ef1b6a3a7051c1675dbb1d7c43d
5yndr0m <sdilanjana21@gmail.com>
2025-03-27 06:03:04 +0530

    build: update

---
+++ next.config.js
@@ -0,0 +1,54 @@
+/ @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['localhost', process.env.NEXT_PUBLIC_API_URL?.replace(/^https?:\/\//, '')],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/',
+      },
+      {
+        protocol: 'https',
+        hostname: process.env.NEXT_PUBLIC_API_URL?.replace(/^https?:\/\//, ''),
+        pathname: '/uploads/*',
+      },
+    ],
+  },
+  // Enable production optimizations
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  // Configure headers for security
+  async headers() {
+    return [
+      {
+        source: '/:path',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on'
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains'
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin'
+          }
+        ]
+      }
+    ];
+  }
+};
+module.exports = nextConfig;
